f2b4467da1dc875934f3088bb269e2b0d0056e41
Mirudistok32 <mirudistok32@yandex.ru>
2020-10-10 14:15:33 +0300

    init

---
+++ src/bll/store.ts
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+const rootReducers = combineReducers({
+
+})
+
+export type AppStateType = ReturnType<typeof rootReducers>
+
+type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never
+export type InferActionsTypes<T extends { [key: string]: (...args: any[]) => any }> = ReturnType<PropertiesTypes<T>>
+
+const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)))
+
+export default store
---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './App.module.scss';
+
+export function App() {
+  return (
+    <div className={s.app}>
+
+    </div>
+  );
+}
