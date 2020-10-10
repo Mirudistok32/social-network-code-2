754db1da30733db8846faba918e0f3e761377ddb
Mirudistok32 <mirudistok32@yandex.ru>
2020-10-10 14:17:34 +0300

    Add Provider store

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components/App/App';
+import { Provider } from 'react-redux';
+import store from './bll/store';
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
