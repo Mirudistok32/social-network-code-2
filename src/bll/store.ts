79b0b987917ff2127c749eae7feeef262fb4aa71
Mirudistok <mirudistok32@yandex.ru>
2020-10-16 21:23:14 +0300

    Create files for all reducers and add new code for type InferActionsTypes

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -7,9 +7,7 @@ const rootReducers = combineReducers({
 })
 
 export type AppStateType = ReturnType<typeof rootReducers>
-
-type PropertiesTypes<T> = T extends { [key: string]: infer U } ? U : never
-export type InferActionsTypes<T extends { [key: string]: (...args: any[]) => any }> = ReturnType<PropertiesTypes<T>>
+export type InferActionsTypes<T> = T extends { [keys: string]: (...args: any[]) => infer U } ? U : never
 
 const store = createStore(rootReducers, composeWithDevTools(applyMiddleware(thunk)))
 
