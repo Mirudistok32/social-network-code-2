3e6e5ec3f7df3d3a0e7b6d529fe19ebb3b95997e
Mirudistok <mirudistok32@yandex.ru>
2020-10-19 15:57:02 +0300

    Create Navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { PATH } from '../../routers/routers';
 import s from './Navigation.module.scss';
 
 type PropsType = {
@@ -12,11 +13,44 @@ export const Navigation: React.FC<PropsType> = React.memo((props) => {
 
   return (
     <div className={s.navigation}>
-      <ul>
-        <li>
-          <NavLink >
+      <ul className={s.navigation__list}>
+        <li className={s.navigation__element}>
+          <NavLink
+            activeClassName={s['navigation__element-link-active']}
+            className={s['navigation__element-link']}
+            to={PATH.profile}>
             <span className="material-icons">
               perm_identity
+            </span>
+          </NavLink>
+        </li>
+        <li className={s.navigation__element}>
+          <NavLink
+            activeClassName={s['navigation__element-link-active']}
+            className={s['navigation__element-link']}
+            to={PATH.messages}>
+            <span className="material-icons">
+              chat
+            </span>
+          </NavLink>
+        </li>
+        <li className={s.navigation__element}>
+          <NavLink
+            activeClassName={s['navigation__element-link-active']}
+            className={s['navigation__element-link']}
+            to={PATH.friends}>
+            <span className="material-icons">
+              people
+            </span>
+          </NavLink>
+        </li>
+        <li className={s.navigation__element}>
+          <NavLink
+            activeClassName={s['navigation__element-link-active']}
+            className={s['navigation__element-link']}
+            to={PATH.search}>
+            <span className="material-icons">
+              person_search
             </span>
           </NavLink>
         </li>
